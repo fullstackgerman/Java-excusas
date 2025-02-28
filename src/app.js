0a85aa972033e4cde180b9407efe3e4da210afa1
victorpallas1 <victorpallas@seguridadea1.com>
2025-02-28 20:54:25 +0000

    victorpallas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+let primero = ['el', 'la', 'a', 'los'];
+let segundo = ['best', 'mejor', 'alto', 'gran' ];
+let tercero = ['coche', 'hardware', 'seguridad', 'moto', 'bicicleta'];
+let dominio = ['.com', '.es', '.gal', '.pro']
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarDominios(){
+  let combinaciones=""
+  for(let a = 0; a < primero.length; a++) {
+    for(let b = 0; b < segundo.length; b++) {
+      for(let c = 0; c < tercero.length; c++) {
+        for(let d = 0; d < dominio.length; d++) {
+          combinaciones+=`<li class="list-groups-item">${primero[a]+segundo[b]+tercero[c]+dominio[d]}</li>`
+        }
+      }
+    }
+  }
+  console.log(combinaciones)
+  return combinaciones
+    
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("domains").innerHTML=generarDominios()
 };
+
+
+for(let a = 0; a < primero.length; a++) {
+  for(let b = 0; b < segundo.length; b++) {
+    for(let c = 0; c < tercero.length; c++) {
+      for(let d = 0; d < dominio.length; d++) {
+      console.log(primero[a] + segundo[b] + tercero[c]+ dominio[d]);
+      }
+    }
+  }
+}
