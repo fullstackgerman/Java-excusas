65b9a05e895c1e8f2b5f73e7b63c97a4a5eb64f8
Robertjsr <Eryero02@gmail.com>
2025-03-03 05:03:46 +0000

    generadorNombres

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/perro inteligente.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
   console.log("Hello Rigo from the console!");
 };
+
+  function generadorDeNombres () {
+    
+    let pronoun = ['the','our'];
+    let adj = ['great','big'];
+    let noun = ['jogger','racoon'];
+    let dot = [ '.com','.org','.gov' ]
+
+    let text1 = pronoun [Math.floor(Math.random() * pronoun.length)]
+    let text2 = adj [Math.floor(Math.random() * adj.length)]
+    let text3 = noun [Math.floor(Math.random() * noun.length)]
+    let text4 = dot [Math.floor(Math.random() * dot.length)]
+
+    let resultado = text1 + " "  + text2 + " " + text3 + " " + text4;
+
+    document.getElementById("excuse").innerHTML = resultado
+  }
+  generadorDeNombres()
+  
+  
