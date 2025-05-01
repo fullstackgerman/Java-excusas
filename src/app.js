47370ea3798b1edac76de9e88f9a611b56f4b5b4
Adri <adrianferrert@gmail.com>
2025-05-01 07:40:29 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("body");
+  console.dir(document.querySelector("#card"));
+  console.log("_______________");
+  document.querySelector("#card")//.algo 
+  // Pensando:
+  // Objeto de objetos? o con arrays o como? 
+  const card = {
+    palo: ['♦' , '♥' , '♠',  '♣'],
+    valor: [1,2,3,4,5,6,7,8,9,10, Jack, Queen, King, As],
+  }
+  const mazo = {
+    value: card,
+    length:52
+    
+  }
+
+
 };
