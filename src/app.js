885b5af1f6dd1cd536e543669ce576d4704ee35a
Betoluchenco <63628143+Betoluchenco@users.noreply.github.com>
2025-05-28 13:40:52 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.onload = function() {
+    let who    = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+    let action = ['se comió', 'me rompió', 'pisó', 'perdió'];
+    let what   = ['mi tarea', 'mi teléfono', 'el coche', 'mi almuerzo'];
+    let when   = ['antes de la clase', 'mientras dormía', 'durante mi almuerzo'];
+
+    function generateExcuse() {
+      const random = arr => arr[Math.floor(Math.random() * arr.length)];
+      return `${random(who)} ${random(action)} ${random(what)} ${random(when)}.`;
+    }
+
+    document.getElementById('excusas').innerHTML = generateExcuse();
 };
