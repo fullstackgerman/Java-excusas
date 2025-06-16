5c2f86a7f6095a0430d5a2409842fdcf8cb83fb9
ECG <cgeulalia@gmail.com>
2025-06-16 18:31:40 +0000

    excusas2

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let generateExcuse = () => { 
+let person = ["my dog", "my grandma", "my turtle", "my dad"];
+let action = ["ate", "forgot", "stole", "broke"];
+let object = ["my homework", "my exam", "my backpack", "my folder"];
+let when= ["yesterday", "last night", "this morning", "a while ago"];
+
+let personIndex = Math.floor(Math.random() * person.length);
+let actionIndex = Math.floor(Math.random() * action.length);
+let objectIndex = Math.floor(Math.random() * object.length);
+let whenIndex = Math.floor(Math.random() * when.length);
+
+ return person[personIndex] + " " + action[actionIndex] + " " + object[objectIndex] + " " + when[whenIndex];
+};
+document.querySelector('#excuse').innerHTML = generateExcuse();
+console.log("Hello Rigo from the console!");
 };
